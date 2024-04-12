064040b7ef6c3b6844e13d9d0dcc527a22bb3da5
rafaelafae <rafaela.fae@gmail.com>
2024-04-12 19:18:35 -0300

    Teste de formulário 2

--- script.js
+++ script.js
@@ -1,59 +1,3 @@
-// class FormSubmit {
-//   constructor(settings) {
-//     this.settings = settings;
-//     this.form = document.querySelector(settings.form);
-//     this.formButton = document.querySelector(settings.button);
-
-//     if (this.form) {
-//       this.url = this.form.getAttribute("action");
-//     }
-//   }
-
-//   displaySuccess() {
-//     this.form.innerHTML = this.settings.success;
-//   }
-
-//   displayError() {
-//     this.form.innerHTML = this.settings.error;
-//   }
-
-//   getFormObject() {
-//     const formObject = document.getElementById("form");
-// const username = document.getElementById("username");
-// const email = document.getElementById("email");
-// const subject = document.getElementById("subject");
-// const message = document.getElementById("message");
-//   }
-
-//   async sendForm() {
-//     try {
-//     await fetch(this.url, {
-//       method: "POST",
-//       headers: {
-//         'Content-Type': 'application/json',
-//         'Accept': 'application/json'
-//       },
-//       body: "",
-//     });
-//     this.displaySuccess();
-//     } catch {
-//       this.displayError(error);
-//     }
-//   }
-
-//   init() {
-//     if (this.form) this.formButton.addEventListener("click", => this.displaySuccess());
-//     return this;
-//   }
-// }
-
-// const FormSubmit = new FormSubmit({
-//   form: "[data-form]",
-//   button: "[data-button]",
-//   sucess: "<h1 class="success">Mensagem enviada!</h1>",
-//   error: "<h1 class="error">Não foi possível enviar sua mensagem.</h1>"
-// });
-
 const form = document.getElementById("form");
 const username = document.getElementById("username");
 const email = document.getElementById("email");
@@ -64,21 +8,38 @@ form.addEventListener("submit", (e) => {
   e.preventDefault();
 
   checkInputs();
+
+  // Ckeck if all form controls have 'success' class
+  const formControls = form.querySelectorAll(".form__control");
+  const formIsValid = [...formControls].every((formControl) => {
+    return formControl.className === "form__control success";
+  });
+
+  if (formIsValid) {
+    console.log("Enviando formulário válido...");
+    // Submit form data here
+    form.submit();
+  } else {
+    // Form is not valid, do nothing or display an error message
+    console.log("O formulário não está válido.");
+  }
 });
 
-// Função para realizar as validações dos inputs
+// Validation inputs
 function checkInputs() {
-  const usernameValue = username.value;
-  const emailValue = email.value;
-  const subjectValue = subject.value;
-  const messageValue = message.value;
+  const usernameValue = username.value.trim();
+  const emailValue = email.value.trim();
+  const subjectValue = subject.value.trim();
+  const messageValue = message.value.trim();
 
+  // Name validation
   if (usernameValue === "") {
     setErrorFor(username, "O nome é obrigatório.");
   } else {
     setSuccessFor(username);
   }
 
+  // Email validation
   if (emailValue === "") {
     setErrorFor(email, "O email é obrigatório.");
   } else if (!checkEmail(emailValue)) {
@@ -87,28 +48,20 @@ function checkInputs() {
     setSuccessFor(email);
   }
 
+  // Subject validation
   if (subjectValue === "") {
     setErrorFor(subject, "O assunto é obrigatório.");
   } else {
     setSuccessFor(subject);
   }
 
+  // Message validation
   if (messageValue === "") {
     setErrorFor(message, "A mensagem é obrigatória.");
   } else if (messageValue.length < 20) {
     setErrorFor(message, "A messagem deve ter no mínimo 20 caracteres.");
   } else {
     setSuccessFor(message);
-  }
-
-  const formControls = form.querySelectorAll(".form__control");
-
-  const formIsValid = [...formControls].every((formControl) => {
-    return formControl.className === "form__control success";
-  });
-
-  if (formIsValid) {
-    console.log("O formulário esta 100% válido!");
   }
 }
 
